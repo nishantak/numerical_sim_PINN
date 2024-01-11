1ece2ab890919c84a99240608a9cb4f56b3f397b
nishantak09 <nishantak09@gmail.com>
2024-01-11 09:16:32 +0530

    .

--- matplot.py
+++ matplot.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -18,25 +17,4 @@ for t in range(time_steps):
     plt.draw()
     plt.pause(0.1)  # Adjust the pause time as needed
 
-=======
-import matplotlib.pyplot as plt
-import numpy as np
-
-# Read simulation data from the file
-data = np.loadtxt('simulation_data.txt')
-
-# Plotting
-x_values = np.linspace(0, 2.0, 200)
-time_steps = len(data)
-plt.xlabel('x')
-plt.ylabel('u(x)')
-
-for t in range(time_steps):
-    plt.clf()
-    plt.title(f'FVM KdV Simulation at Time Step {t}')
-    plt.plot(x_values, data[t, :])
-    plt.draw()
-    plt.pause(0.1)  # Adjust the pause time as needed
-
->>>>>>> master
 plt.show()
