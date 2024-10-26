a36e9783662174e76347ce033e5938b7b911127c
nishantak <nishantak09@gmail.com>
2024-10-26 17:45:58 +0530

    smthing

--- PINN_Kuramoto/steps/gif.py
+++ PINN_Kuramoto/steps/gif.py
@@ -1,6 +1,6 @@
 import glob
 
-steps = sorted(glob.glob(r"*.png"), key=lambda x: int(x.split(".png")[0]))
+steps = sorted(glob.glob(r"steps/*.png"), key=lambda x: int(x.split(".png")[0].split("\\")[1]))
 
 from PIL import Image
 
@@ -9,4 +9,4 @@ for step in steps:
     frame = Image.open(step)
     frames.append(frame)
 
-frames[0].save("output.gif", save_all=True, append_images=frames[1:], loop=0, duration=20)
+frames[0].save("output.gif", save_all=True, append_images=frames[1:], loop=0, duration=1000)
